24dbea6bb94d9e9769832de3772156bb00c7ce1e
vanshkataria027-del <vanshkataria027@gmail.com>
2026-09-04 13:28:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ st.set_page_config(
 st.title("🤖 My AI Chatbot")
 st.caption("Powered by Gemini")
 
-# Gemini API key
-genai.configure(api_key=st.secrets["AQ.Ab8RN6Kd4PvDeAiaDNxs-4FXQy93mXbKKINck6M17Xs-EBU4-Qs"])
+# Gemini
+client = genai.Client(
+    api_key=st.secrets["GEMINI_API_KEY"]
+)
 
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = "gemini-3-flash-preview"
 
 # Chat history
 if "messages" not in st.session_state:
@@ -24,26 +25,26 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Chat input
-user_input = st.chat_input("Type your message...")
+# User input
+if prompt := st.chat_input("Type your message..."):
 
-if user_input:
-    st.session_state.messages.append({
-        "role": "user",
-        "content": user_input
-    })
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
 
     with st.chat_message("user"):
-        st.markdown(user_input)
+        st.markdown(prompt)
+
+    response = client.models.generate_content(
+        model=model,
+        contents=prompt
+    )
+
+    answer = response.text
 
     with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = model.generate_content(user_input)
-            answer = response.text
-
         st.markdown(answer)
 
-    st.session_state.messages.append({
-        "role": "assistant",
-        "content": answer
-    })
+    st.session_state.messages.append(
+        {"role": "assistant", "content": answer}
+    )
